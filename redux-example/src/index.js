0ed970daf819fbf539d9d45e1f375122fe925115
Ivanb <iv.bitkin@gmail.com>
2018-12-13 21:04:56 +0200

    added reducer for my project

--- redux-example/src/index.js
+++ redux-example/src/index.js
@@ -1,13 +1,31 @@
 import { createStore } from "redux";
 
-function playlist(state = []) {
+function playlist(state = [], action) {
+  if (action.type === "ADD_TRACK") {
+    return [...state, action.payload];
+  }
   return state;
 }
 
 const store = createStore(playlist);
 
+const addTrackBtn = document.querySelectorAll(".addTrack")[0];
+const trackInput = document.querySelectorAll(".trackInput")[0];
+const list = document.querySelectorAll(".list")[0];
+
 store.subscribe(() => {
-  console.log("subscribe", store.getState());
+  list.innerHTML = "";
+  trackInput.value = "";
+  store.getState().forEach(track => {
+    const li = document.createElement("li");
+    li.textContent = track;
+    list.appendChild(li);
+  });
 });
 
-store.dispatch({ type: "ADD_TRACK", payload: "Rap God" });
+// store.dispatch({ type: "ADD_TRACK", payload: "Rap God" });
+
+addTrackBtn.addEventListener("click", () => {
+  const trackName = trackInput.value;
+  store.dispatch({ type: "ADD_TRACK", payload: trackName });
+});
