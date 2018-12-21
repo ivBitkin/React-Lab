592cc780bca644085d6e37e36ed754cf9cb84827
Ivanb <iv.bitkin@gmail.com>
2018-12-21 12:40:13 +0200

    added thunk

--- react-redux-firebase/src/store/reducers/projectReducer.js
+++ react-redux-firebase/src/store/reducers/projectReducer.js
@@ -6,7 +6,11 @@ const initState = {
   ]
 };
 
-const projectReducer = (state = initState, actiom) => {
+const projectReducer = (state = initState, action) => {
+  switch (action.type) {
+    case "CREATE_PROJECT":
+      console.log("created project", action.project);
+  }
   return state;
 };
 
